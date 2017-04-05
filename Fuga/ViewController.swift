c6dd16c7978c66833de9e293e81cc5a00fa3f416
tanaka.kenji <tanaka.kenji@ip-172-17-0-130.ap-northeast-1.compute.internal>
2017-04-05 12:30:31 +0900

    Initial Commit

---
+++ Fuga/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fuga
+//
+//  Created by tanaka.kenji on 2017/04/05.
+//  Copyright © 2017年 tanaka.kenji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
